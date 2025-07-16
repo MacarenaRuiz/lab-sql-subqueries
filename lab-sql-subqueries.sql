60622dccade73c49a34ca646f2d85ffdcf2d26e6
Macarena <macaruiz4@hotmail.com>
2025-07-16 23:43:44 +0200

    lab

---
+++ lab-sql-subqueries.sql
@@ -0,0 +1,6 @@
+USE sakila;
+-- 1. Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+
+-- 2. List all films whose length is longer than the average length of all the films in the Sakila database.
+
+-- 3. Use a subquery to display all actors who appear in the film "Alone Trip".
